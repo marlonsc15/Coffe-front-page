49bd7bf01c95ef729cf05ebcfc6d09f98141ffcc
marlonsc <marlonsantoscorreia@gmail.com>
2022-06-27 14:59:47 -0300

    melhorias na navbar mobile

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
+const A = document.getElementsByClassName('A')[0]
 
 
 toggleButton.addEventListener('click', () => {
@@ -8,7 +8,10 @@ toggleButton.addEventListener('click', () => {
 })
 
 
+function refresh() {
+    return location.reload()
+}
 
-
-
-
+A.addEventListener('click', () => {
+  window.location.href = "http://conteudo.imguol.com.br/c/entretenimento/ac/2020/08/14/cafe-especial-1597429133880_v2_1920x1280.jpg";
+})
